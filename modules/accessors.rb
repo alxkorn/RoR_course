6264c3c3ee5825170440c97499c36a2be0d584e3
alxkorn <akorneyev10@yandex.ru>
2019-09-19 16:58:41 +0300

    added Accessors module

--- modules/accessors.rb
+++ modules/accessors.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Accessors
   def self.included(base)
     base.extend ClassMethods
@@ -5,23 +7,31 @@ module Accessors
   end
 
   module ClassMethods
-    
-    def my_attr_accessor(*names)
+    def attr_accessor_with_history(*names)
       names.each do |name|
+        hist_reader_sym = "#{name}_history".to_sym
+        hist_var_name = "@#{hist_reader_sym}".to_sym
         var_name = "@#{name}".to_sym
-        define_method(name) { instance_variable_get(var_name) }
-        attr_reader name
-        define_method("#{name}=".to_sym) { |value| instance_variable_set(var_name, value) }
+        attr_reader hist_reader_sym, name
+        define_method("#{name}=".to_sym) do |value|
+          instance_variable_set(hist_var_name, []) if instance_variable_get(hist_var_name).nil?
+          instance_variable_get(hist_var_name) << value
+          instance_variable_set(var_name, value)
+        end
+      end
+    end
+
+    def strong_attr_accessor(name, class_name)
+      var_name = "@#{name}".to_sym
+      attr_reader name
+      define_method("#{name}=".to_sym) do |value|
+        raise ArgumentError unless value.is_a? class_name
+
+        instance_variable_set(var_name, value)
       end
     end
   end
 
   module InstanceMethods
   end
-  
-end
-
-class Test
-  include Accessors
-  my_attr_accessor :a
 end
